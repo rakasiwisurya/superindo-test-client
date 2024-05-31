40eb6d08519c70b85fb680b53180d42d4d9a76f5
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2024-05-31 11:31:35 +0700

    done setup redux persist

--- src/redux/app/store.ts
+++ src/redux/app/store.ts
@@ -1,4 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
 import {
   authReducer,
   cartReducer,
@@ -9,23 +10,36 @@ import {
   transactionReducer,
   userReducer,
 } from "../features";
+import { persistReducer, persistStore } from "redux-persist";
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    dashboard: dashboardReducer,
-    productCategory: productCategoryReducer,
-    product: productReducer,
-    productVariant: productVariantReducer,
-    transaction: transactionReducer,
-    cart: cartReducer,
-    user: userReducer,
-  },
-  devTools: process.env.NODE_ENV === "development",
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+const persistConfig = {
+  key: "root",
+  storage,
+  // whitelist: [],
+  // blacklist: [],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  dashboard: dashboardReducer,
+  productCategory: productCategoryReducer,
+  product: productReducer,
+  productVariant: productVariantReducer,
+  transaction: transactionReducer,
+  cart: cartReducer,
+  user: userReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
